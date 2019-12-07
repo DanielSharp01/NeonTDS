a9698b609300abbe750d4e0dbe969ee27ba78c56
DanielSharp01 <DanielSharp01@gmail.com>
2019-12-07 19:10:13 +0100

    Missing file

---
+++ Networking/Messages/InputAckMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NeonTDS
+{
+    public class InputAckMessage : Message
+    {
+        public uint SequenceNumber { get; set; }
+        public InputAckMessage()
+            : base(MessageTypes.PlayerInputAck)
+        { }
+
+        public override void FromBytes(BinaryReader reader)
+        {
+            base.FromBytes(reader);
+            SequenceNumber = reader.ReadUInt32();
+        }
+
+        public override void ToBytes(BinaryWriter writer)
+        {
+            base.ToBytes(writer);
+            writer.Write(SequenceNumber);
+        }
+    }
+}
